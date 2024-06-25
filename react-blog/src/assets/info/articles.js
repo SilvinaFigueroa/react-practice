047db7cc74409594585610ec79037f7cb8b8eb73
Silvina Figueroa <silvina.sfigueroa@gmail.com>
2024-06-25 09:46:06 -0700

    import statement for images

--- react-blog/src/assets/info/articles.js
+++ react-blog/src/assets/info/articles.js
@@ -1,4 +1,5 @@
-
+import image1 from '../assets/images/blog-image-1.jpg';
+import image2 from '../assets/images/blog-image-2.jpg';
 
 let articleInfo = [{
     datetime: "2020/12/11",
@@ -11,7 +12,7 @@ let articleInfo = [{
           amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed
           nulla eveniet doloribus neque optio odio qui! Id architecto impedit consequatur rem quaerat voluptatibus pariatur
           quas,quod quisquam quo ab molestiae.`,
-    image : "/src/assets/images/blog-image-1.jpg",
+    image : image1,
     imageAlt : "woman in red"
 },
 {
@@ -24,7 +25,7 @@ let articleInfo = [{
         corrupti optio atque ut. Repellendus molestiae doloribus recusandae, itaque voluptatibus placeat repellat deserunt
         amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed
         Fugit, cum, cupiditate aliquam, mollitia quisquam sed nulla eveniet doloribus neque optio odio qui! quas,quod quisquam quo ab molestiae.`,
-    image : "/src/assets/images/blog-image-2.jpg",
+    image : image2,
     imageAlt : "blue walls"
 }
 ]
