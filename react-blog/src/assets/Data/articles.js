8f2233e032e6ebfe4fb421e31f412a7c209299f8
Silvina Figueroa <silvina.sfigueroa@gmail.com>
2024-06-24 22:14:00 -0700

    "updating image path"

--- react-blog/src/assets/Data/articles.js
+++ react-blog/src/assets/Data/articles.js
@@ -1,5 +1,4 @@
-import image1 from '../assets/blog-image-1.jpg';
-import image2 from '../assets/blog-image-2.jpg';
+
 
 
 let articleInfo = [{
@@ -13,7 +12,7 @@ let articleInfo = [{
           amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed
           nulla eveniet doloribus neque optio odio qui! Id architecto impedit consequatur rem quaerat voluptatibus pariatur
           quas,quod quisquam quo ab molestiae.`,
-    image : image1,
+    image : "./src/assets/Data/blog-image-1.jpg",
     imageAlt : "woman in red"
 },
 {
@@ -26,7 +25,7 @@ let articleInfo = [{
         corrupti optio atque ut. Repellendus molestiae doloribus recusandae, itaque voluptatibus placeat repellat deserunt
         amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed
         Fugit, cum, cupiditate aliquam, mollitia quisquam sed nulla eveniet doloribus neque optio odio qui! quas,quod quisquam quo ab molestiae.`,
-    image : image2,
+    image : "./src/assets/Data/blog-image-2.jpg",
     imageAlt : "blue walls"
 }
 ]
