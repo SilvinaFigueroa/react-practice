0fa2cee28c0f0600afa1d1e6aa6c6d44b331a094
Silvina Figueroa <silvina.sfigueroa@gmail.com>
2024-06-24 21:55:20 -0700

    "fixing image name"

--- react-blog/src/assets/Data/articles.js
+++ react-blog/src/assets/Data/articles.js
@@ -10,7 +10,7 @@ let articleInfo = [{
           amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed
           nulla eveniet doloribus neque optio odio qui! Id architecto impedit consequatur rem quaerat voluptatibus pariatur
           quas,quod quisquam quo ab molestiae.`,
-    image : "./src/assets/blog-image-1.jpg",
+    image : "../assets/blog-image-1.jpg",
     imageAlt : "woman in red"
 },
 {
@@ -22,7 +22,9 @@ let articleInfo = [{
         impedit voluptas aut voluptates facere totam aliquid debitis rerum, ex consequuntur, nulla eius quae
         corrupti optio atque ut. Repellendus molestiae doloribus recusandae, itaque voluptatibus placeat repellat deserunt
         amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed
-        Fugit, cum, cupiditate aliquam, mollitia quisquam sed nulla eveniet doloribus neque optio odio qui! quas,quod quisquam quo ab molestiae.`
+        Fugit, cum, cupiditate aliquam, mollitia quisquam sed nulla eveniet doloribus neque optio odio qui! quas,quod quisquam quo ab molestiae.`,
+    image : "../assets/blog-image-2.jpg",
+    imageAlt : "blue walls"
 }
 ]
 
