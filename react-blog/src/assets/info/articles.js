269179ef05fed1d7240df2828949c1772282e2ff
Silvina Figueroa <silvina.sfigueroa@gmail.com>
2024-06-25 09:51:50 -0700

    fixing relative path

--- react-blog/src/assets/info/articles.js
+++ react-blog/src/assets/info/articles.js
@@ -1,5 +1,3 @@
-import image1 from '../images/blog-image-1.jpg';
-import image2 from '../images/blog-image-2.jpg';
 
 let articleInfo = [{
     datetime: "2020/12/11",
@@ -12,7 +10,7 @@ let articleInfo = [{
           amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed
           nulla eveniet doloribus neque optio odio qui! Id architecto impedit consequatur rem quaerat voluptatibus pariatur
           quas,quod quisquam quo ab molestiae.`,
-    image : image1,
+    image : '../images/blog-image-1.jpg',
     imageAlt : "woman in red"
 },
 {
@@ -25,7 +23,7 @@ let articleInfo = [{
         corrupti optio atque ut. Repellendus molestiae doloribus recusandae, itaque voluptatibus placeat repellat deserunt
         amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed
         Fugit, cum, cupiditate aliquam, mollitia quisquam sed nulla eveniet doloribus neque optio odio qui! quas,quod quisquam quo ab molestiae.`,
-    image : image2,
+    image : "../images/blog-image-2.jpg",
     imageAlt : "blue walls"
 }
 ]
